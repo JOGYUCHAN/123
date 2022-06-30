9f38efc24f84d1d82a925551cdee8225ef0756b0
조규찬 <emfohrs2234@naver.com>
2022-06-30 16:53:48 +0900

    crawling

--- 1.py
+++ 1.py
@@ -2,12 +2,21 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-response = requests.get("http://www.pigtimes.co.kr/news/articleView.html?idxno=45814")
+response = requests.get("http://www.pigtimes.co.kr/news/articleView.html?idxno=45813")
 html = response.text
 soup = BeautifulSoup(html, 'html.parser')
+links2 = soup.select_one(".article-head-nav>a").get_text()
+# links2 = soup.find("a",{"href" : "/news/articleList.html?sc_section_code=S1N14"}).find_all(text = True)
+# print(links2)
 
-links2 = soup.select("#article-view-content-div")
-for link2 in links2:
-    article = link2.text
+print(links2)
 
-print(article)
+# for link2 in links2:
+#     article = link2.text
+
+# a = article.replace("\n","").replace("저작권자 © 양돈타임스 무단전재 및 재배포 금지양돈타임스다른기사 보기","")
+# a.rstrip("©")
+# print(a)
+
+# f = open("test.txt","w",encoding='utf8')
+# f.write(a)
--- crawling.py
+++ crawling.py
@@ -3,14 +3,17 @@ from bs4 import BeautifulSoup
 import pandas as pd
 
 a = []
-for i in range(14,30):
-    response = requests.get(f"http://www.pigtimes.co.kr/news/articleView.html?idxno=458{i}")
+for i in range(600,800):
+    response = requests.get(f"http://www.pigtimes.co.kr/news/articleView.html?idxno=45{i}")
+    url = f"http://www.pigtimes.co.kr/news/articleView.html?idxno=45{i}"
     html = response.text
-    if ("관리자가 검토중인 기사 입니다" in html) :
-        break
-    else :
-        soup = BeautifulSoup(html, 'html.parser')
+    soup = BeautifulSoup(html, 'html.parser')
 
+    if("관리자가 검토중인 기사 입니다." in html) or("존재하지 않는 링크" in html):
+        continue
+    else:
+        name = soup.select_one(".article-head-nav>a").get_text()
+        theme = name
         links3 = soup.select(".updated")
         for link3 in links3:
             time = link3.text
@@ -21,12 +24,15 @@ for i in range(14,30):
 
         links2 = soup.select("#article-view-content-div")
         for link2 in links2:
-            article = link2.text
-        
-        a.append([time,title,article])
+            arti = link2.text
+            article = arti.replace("\n","").replace(" 저작권자 © 양돈타임스 무단전재 및 재배포 금지김현구다른기사 보기 ","")
+
+    a.append([time, theme ,url, title, article])
+
+df = pd.DataFrame(a,columns=['time','theme','url','title','article'])
+df.to_csv("news.csv",index=False, encoding='utf8')
+
     
-df = pd.DataFrame(a, columns=['time','title', 'article'])
-df.to_csv('news.csv', index=False,encoding='utf8') 
 
 
     
